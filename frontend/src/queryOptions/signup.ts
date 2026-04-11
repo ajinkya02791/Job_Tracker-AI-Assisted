a6d39a66ef06c0ceddeb38dea98e92ec3771433f
ajinkya <77152422+ajinkya02791@users.noreply.github.com>
2026-04-11 17:51:13 +0000

    files location changed

--- frontend/src/queryOptions/signup.ts
+++ frontend/src/queryOptions/signup.ts
@@ -3,11 +3,10 @@ import axios from 'axios'
 
 export const signupUser = async (data: SignupData) => {
   try {
-    const res = await axios.post("http:localhost:5000/api", data);
+    const res = await axios.post("http://localhost:5000/api/auth/signup", data);
     const result = res.data;
     return result;
   } catch (error: any) {
-    
     alert(`${error.message}` || "Signup failed");
   }
 };
